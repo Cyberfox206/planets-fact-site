258318cbaa1982d994a3abc5dcadb0022fef8cd8
Cyberfox206 <bbrontesi@edenschool.fr>
2023-06-01 17:15:52 +0200

    start inteligency

---
+++ app.js
@@ -0,0 +1,38 @@
+let img_mercury = document.getElementById("img_mercury");
+let img_venus = document.getElementById("img_venus");
+let img_earth = document.getElementById("img_earth");
+let img_mars = document.getElementById("img_mars");
+let img_jupiter = document.getElementById("img_jupiter");
+let img_saturn = document.getElementById("img_saturn");
+let img_uranus = document.getElementById("img_uranus");
+let img_neptune = document.getElementById("img_neptune");
+
+let btn_mercury_overview = document.getElementById("btn_mercury_overview");
+let btn_venus_overview = document.getElementById("btn_venus_overview");
+let btn_earth_overview = document.getElementById("btn_earth_overview");
+let btn_mars_overview = document.getElementById("btn_mars_overview");
+let btn_jupiter_overview = document.getElementById("btn_jupiter_overview");
+let btn_saturn_overview = document.getElementById("btn_saturn_overview");
+let btn_uranus_overview = document.getElementById("btn_uranus_overview");
+let btn_neptune_overview = document.getElementById("btn_neptune_overview");
+
+let btn_mercury_structure = document.getElementById("btn_mercury_structure");
+let btn_venus_structure = document.getElementById("btn_venus_structure");
+let btn_earth_structure = document.getElementById("btn_earth_structure");
+let btn_mars_structure = document.getElementById("btn_mars_structure");
+let btn_jupiter_structure = document.getElementById("btn_jupiter_structure");
+let btn_saturn_structure = document.getElementById("btn_saturn_structure");
+let btn_uranus_structure = document.getElementById("btn_uranus_structure");
+let btn_neptune_structure = document.getElementById("btn_neptune_structure");
+
+let btn_mercury_surface = document.getElementById("btn_mercury_surface");
+let btn_venus_surface = document.getElementById("btn_venus_surface");
+let btn_earth_surface = document.getElementById("btn_earth_surface");
+let btn_mars_surface = document.getElementById("btn_mars_surface");
+let btn_jupiter_surface = document.getElementById("btn_jupiter_surface");
+let btn_saturn_surface = document.getElementById("btn_saturn_surface");
+let btn_uranus_surface = document.getElementById("btn_uranus_surface");
+let btn_neptune_surface = document.getElementById("btn_neptune_surface");
+
+
+console.log(btn_mercury_overview)
