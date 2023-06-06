a02a2b9e3ea1c49d283230cc7ab8de0b9db20c8f
Cyberfox206 <bbrontesi@edenschool.fr>
2023-06-06 17:14:39 +0200

    start génération data of the js

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
+let all_btn_planet = document.getElementsByClassName("all_btn_planet");
+let all_planet_img = document.getElementById("all_planet_img");
+let planet_desc_text = document.getElementById("planet_desc_text")
+let information_all_planet = document.getElementsByClassName("information_all_planet");
 
-
-const base_url = window.location.href;
-console.log(base_url)
-switch (base_url) {
-    case base_url:
-
-        break;
-
-    default:
-        break;
+const js_all_planet = {
+    mercury: {
+        mercury_color: 'mercury_color',
+        mercury_planet_img: ['assets/planet-mercury.svg', 'assets/planet-mercury-internal.svg', 'psedo'],
+        mercury_desc_text: `Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes
+                            87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar
+                            System, and is a rocky body like Earth.`,
+        mercury_info: ['58.6 day', '87.97 days', '2,439.7 km', '430°c'],
+    }
 }
 
-
-
-
-let mercury_btn_tabl = document.getElementsByClassName("mercury_btn");
-let mercury_img = document.getElementById("mercury_img")
-
-function mercury_inteligency(mercury_btn_tabl, mercury_img) {
-    let last_select = mercury_btn_tabl[0];
-    for (let i = 0; i < mercury_btn_tabl.length; i++) {
-        mercury_btn_tabl[i].addEventListener("click", () => {
-            last_select.classList.remove("mercurycolor");
+function switch_btn_and_image(table_btn, color, tab_image) {
+    let last_select = table_btn[0];
+    for (let i = 0; i < table_btn.length; i++) {
+        table_btn[i].addEventListener("click", () => {
             last_select.classList.remove("border-bottom");
-            last_select.classList.remove("mercury_geology");
-            mercury_btn_tabl[i].classList.add("mercurycolor");
-            mercury_btn_tabl[i].classList.add("border-bottom");
-
-            if (mercury_btn_tabl[i].dataset.modif_img === "psedo") {
-                mercury_img.classList.add("psedo");
-                mercury_img.style.background = "center / cover url(assets/planet-mercury.svg)";
-            } else {
-                mercury_img.classList.remove("psedo");
-                mercury_img.style.background = `center / cover url(${mercury_btn_tabl[i].dataset.modif_img})`;
-            }
-
-            last_select = mercury_btn_tabl[i];
+            table_btn[i].classList.add("border-bottom");
+            table_btn[i].classList.add(color)
+            all_planet_img.src = tab_image[table_btn[i].dataset.modif_img];
+            last_select = table_btn[i]
         })
     }
 }
-mercury_inteligency(mercury_btn_tabl, mercury_img);
+
+const base_url = window.location.href;
+
+switch (base_url) {
+    case base_url:
+        let color_planet = js_all_planet.mercury.mercury_color;
+        let planet_img = js_all_planet.mercury.mercury_planet_img;
+        all_planet_img.src = js_all_planet.mercury.mercury_planet_img[0];
+        planet_desc_text.textContent = js_all_planet.mercury.mercury_desc_text;
+        for (let i = 0; i < information_all_planet.length; i++) {
+            information_all_planet[i].textContent = js_all_planet.mercury.mercury_info[i];
+        }
+        switch_btn_and_image(all_btn_planet, color_planet, planet_img);
+        break;
+}
