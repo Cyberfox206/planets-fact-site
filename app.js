df485ce20b8e46a44313606207423f83a1812f85
Cyberfox206 <bbrontesi@edenschool.fr>
2023-06-08 14:54:08 +0200

    finish all planet mobile vue

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const js_all_planet = {
         earth_desc_text: `Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.`,
         earth_info: ['0.99 days', '365.26 days', '6,371 km', '16°c'],
     },
-    mars: {
+    jupiter: {
         mars_titre: 'mars',
         mars_color: 'mars_color',
         mars_planet_img: ['assets/planet-mars.svg', 'assets/planet-mars-internal.svg', 'psedo_mars'],
@@ -46,32 +46,40 @@ const js_all_planet = {
         mars_info: ['1.03 days', '1.88 years', '3,389.5 km', '−28°cs']
     },
     jupiter: {
-        ___titre: '',
-        ___color: '',
-        ___planet_img: [],
-        ___desc_text: ``,
-        ___info: [],
+        jupiter_titre: 'jupiter',
+        jupiter_color: 'jupiter_color',
+        jupiter_planet_img: ['assets/planet-jupiter.svg', 'assets/planet-jupiter-internal.svg', 'psedo_jupiter'],
+        jupiter_desc_text: `Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass two
+                        and a half times that of all the other planets in the Solar System combined, but less than one-thousandth the
+                        mass of the Sun.`,
+        jupiter_info: ['9.93 hours', '11.86 years', '69,911 km', '-108°c'],
     },
     saturn: {
-        ___titre: '',
-        ___color: '',
-        ___planet_img: [],
-        ___desc_text: ``,
-        ___info: [],
+        saturn_titre: 'saturn',
+        saturn_color: 'saturn_color',
+        saturn_planet_img: ['assets/planet-saturn.svg', 'assets/planet-saturn-internal.svg', 'psedo_saturn'],
+        saturn_desc_text: `Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas
+                            giant with an average radius of about nine and a half times that of Earth. It only has one-eighth the average
+                            density of Earth.`,
+        saturn_info: ['10.8 hours', '29.46 years', '58,232 km', '-138°c'],
     },
     uranus: {
-        ___titre: '',
-        ___color: '',
-        ___planet_img: [],
-        ___desc_text: ``,
-        ___info: [],
+        uranus_titre: 'uranus',
+        uranus_color: 'uranus_color',
+        uranus_planet_img: ['assets/planet-uranus.svg', 'assets/planet-uranus-internal.svg', 'psedo_uranus'],
+        uranus_desc_text: ` Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky, Uranus according
+                            to Greek mythology, was the great-grandfather of Ares. It has the third-largest planetary radius and
+                            fourth-largest planetary mass in the Solar System.`,
+        uranus_info: ['17.2 hours', '84 years', '25,362 km', '-195°c'],
     },
     neptune: {
-        ___titre: '',
-        ___color: '',
-        ___planet_img: [],
-        ___desc_text: ``,
-        ___info: [],
+        neptune_titre: 'neptune',
+        neptune_color: 'neptune_color',
+        neptune_planet_img: ['assets/planet-neptune.svg', 'assets/planet-neptune-internal.svg', 'psedo_neptune'],
+        neptune_desc_text: `Neptune is the eighth and farthest-known Solar planet from the Sun. In the Solar System, it is the
+                  fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times
+                 the mass of Earth, more massive than its near-twin Uranus.`,
+        neptune_info: ['16.08 hours', '164.79 years', '24,622 km', '-201°c'],
     },
 }
 
@@ -150,15 +158,47 @@ switch (base_url) {
         break;
 
     case 'jupiter':
-
+        titre_planet.textContent = js_all_planet.jupiter.jupiter_titre;
+        color_planet = js_all_planet.jupiter.jupiter_color;
+        planet_img = js_all_planet.jupiter.jupiter_planet_img;
+        all_planet_img.src = js_all_planet.jupiter.jupiter_planet_img[0];
+        planet_desc_text.textContent = js_all_planet.jupiter.jupiter_desc_text;
+        for (let i = 0; i < information_all_planet.length; i++) {
+            information_all_planet[i].textContent = js_all_planet.jupiter.jupiter_info[i];
+        }
+        switch_btn_and_image(all_btn_planet, color_planet, planet_img);
         break
     case 'saturn':
-
+        titre_planet.textContent = js_all_planet.saturn.saturn_titre;
+        color_planet = js_all_planet.saturn.saturn_color;
+        planet_img = js_all_planet.saturn.saturn_planet_img;
+        all_planet_img.src = js_all_planet.saturn.saturn_planet_img[0];
+        planet_desc_text.textContent = js_all_planet.saturn.saturn_desc_text;
+        for (let i = 0; i < information_all_planet.length; i++) {
+            information_all_planet[i].textContent = js_all_planet.saturn.saturn_info[i];
+        }
+        switch_btn_and_image(all_btn_planet, color_planet, planet_img);
         break
     case 'uranus':
-
+        titre_planet.textContent = js_all_planet.uranus.uranus_titre;
+        color_planet = js_all_planet.uranus.uranus_color;
+        planet_img = js_all_planet.uranus.uranus_planet_img;
+        all_planet_img.src = js_all_planet.uranus.uranus_planet_img[0];
+        planet_desc_text.textContent = js_all_planet.uranus.uranus_desc_text;
+        for (let i = 0; i < information_all_planet.length; i++) {
+            information_all_planet[i].textContent = js_all_planet.uranus.uranus_info[i];
+        }
+        switch_btn_and_image(all_btn_planet, color_planet, planet_img);
         break
     case 'neptune':
-
+        titre_planet.textContent = js_all_planet.neptune.neptune_titre;
+        color_planet = js_all_planet.neptune.neptune_color;
+        planet_img = js_all_planet.neptune.neptune_planet_img;
+        all_planet_img.src = js_all_planet.neptune.neptune_planet_img[0];
+        planet_desc_text.textContent = js_all_planet.neptune.neptune_desc_text;
+        for (let i = 0; i < information_all_planet.length; i++) {
+            information_all_planet[i].textContent = js_all_planet.neptune.neptune_info[i];
+        }
+        switch_btn_and_image(all_btn_planet, color_planet, planet_img);
         break
 }
